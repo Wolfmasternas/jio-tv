954af71c218b7b198201bdf09912e7ee37b18218
Wolfmasternas <98648610+Wolfmasternas@users.noreply.github.com>
2022-03-31 22:12:54 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,269 +1,37 @@
-<!DOCTYPE html><html><head>
-  
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>wolftvOfficial</title>
-
-
-<style>
-
-.buttoncs {
-  background-color: #004A7F;
-  -webkit-border-radius: 10px;
-  border-radius: 30px;
-  border: 1px solid #4CAF50 ;
-border-style: dotted dashed solid double;
-
-  color: #FFFFFF;
-  cursor: pointer;
-  display: inline-block;
-  font-family: 'Holtwood One SC', serif;
-  font-size: 20px;
-  background-image: linear-gradient(315deg, #fff000 0%, #d83c3c 74%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  padding: 0px 0px 0px 0px;
-  margin-top:0px;
-  margin-bottom:0px;
-  text-align: center;
-  text-decoration: none;
-  -webkit-animation: glowing 1500ms infinite;
-  -moz-animation: glowing 1500ms infinite;
-  -o-animation: glowing 1500ms infinite;
-  animation: glowing 1500ms infinite;
-}
-@-webkit-keyframes glowing {
-  0% { background-color: #B20000; -webkit-box-shadow: 0 0 3px #B20000; }
-  50% { background-color: #FF0000; -webkit-box-shadow: 0 0 40px #FF0000; }
-  100% { background-color: #B20000; -webkit-box-shadow: 0 0 3px #B20000; }
-}
-
-@-moz-keyframes glowing {
-  0% { background-color: #B20000; -moz-box-shadow: 0 0 3px #B20000; }
-  50% { background-color: #FF0000; -moz-box-shadow: 0 0 40px #FF0000; }
-  100% { background-color: #B20000; -moz-box-shadow: 0 0 3px #B20000; }
-}
-
-@-o-keyframes glowing {
-  0% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-  50% { background-color: #FF0000; box-shadow: 0 0 40px #FF0000; }
-  100% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-}
-
-@keyframes glowing {
-  0% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-  50% { background-color: #FF0000; box-shadow: 0 0 40px #FF0000; }
-  100% { background-color: #B20000; box-shadow: 0 0 3px #B20000; }
-}
-  
-
-
-h5{ 
-
-
-
-
-	text-shadow: 1px 1px 1px white;
-background-color: rgb(150, 150, 150);
-width:70%; 
-	font-size: 16px;
-	line-height: 16px;
-margin: 3px 3px 3px 3px;
-	padding: 10px 15px;
-	border: 1px solid #4CAF50 ;
-	outline: 1px;
-	font-weight: 900;
-	cursor: pointer;
-	border-radius: 34px;
-	transition: 0.25s;
-border-style: dotted dashed solid double;
-
-
-
- }
-
-
-
-button{
-	font-size: 16px;
-	line-height: 16px;
-margin: 3px 3px 3px 3px;
-	padding: 10px 15px;
-	border: 1px solid #4CAF50 ;
-	outline: 1px;
-	font-weight: 900;
-	cursor: pointer;
-	border-radius: 34px;
-	transition: 0.25s;
-border-style: dotted dashed solid double;
-
- 
-}
-
-button.neu-light{
-	background-color: rgb(247, 239, 244);
-	filter: drop-shadow(3.4px 3.4px 3.4px rgb(183, 177, 181)) drop-shadow(-3.4px -3.4px 3.4px white);
-	-webkit-filter: drop-shadow(3.4px 3.4px 3.4px rgb(183, 177, 181)) drop-shadow(-3.4px -3.4px 3.4px white);
-	box-shadow: inset 4.3px 4.3px 7px #fff;
-}
-button.neu-light:hover{
-	filter: drop-shadow(5px 5px 3.4px rgb(183, 177, 181));
-	-webkit-filter: drop-shadow(5px 5px 3.4px rgb(183, 177, 181));
-	box-shadow: inset 2.5px 2.5px 2.5px #fff;
-}
-button.neu-light:active{
-	filter: drop-shadow(0px 0px 0px rgb(183, 177, 181));
-	-webkit-filter: drop-shadow(0px 0px 0px rgb(183, 177, 181));
-	box-shadow: inset 4.3px 4.3px 7px rgb(183, 177, 181);
-	
-}
-button.neu-light.active{
-	filter: unset;
-	box-shadow: unset;
-	filter: drop-shadow(3px 1px 18px rgb(255, 255, 255));
-	-webkit-filter: drop-shadow(3px 1px 18px rgb(255, 255, 255));
-    box-shadow: inset 5.3px 5.3px 7px rgba(183, 177, 181, 0.34);
-    border: 1px solid rgb(255, 255, 255);
-	color: #666;
-	text-shadow: 1px 1px 1px white;
-}
-.btn-action{
-	color: #444;
-}
-.btn-action:hover{
-	color: #321;
-}
-
-</style>
-<script>
-  
-  function changeText() {
-    document.getElementById("btn").innerHTML = "satisfaction";
-  }
-  
-</script> 
-
-
-
-
-
-
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
+<title>JioTV Web</title>
+<link rel="shortcut icon" type="image/x-icon" href="https://i.ibb.co/37fVLxB/f4027915ec9335046755d489a14472f2.png">
+<meta name="robots" content="noindex" />
+<link rel="stylesheet" href="assets/css/darkmode.min.css">
+<script src="https://cdn.jsdelivr.net/npm/lazysizes@5.3.2/lazysizes.min.js"></script>
 </head>
 <body>
-
-<a class="buttoncs" href="https://cdn.wolftvOfficial.ga/jointelegram" style="width:100%;padding:0px 0px 0px 0px;margin:0px 0px 0px 0px; ">
-<h2 style="font-size: 30px;padding:0px 0px 0px 0px;margin:0px 0px 0px 0px;color: coral;text-shadow: -1px 0 black, 0 1px black, 1px 0 red, 0 -1px green;"><img src="https://telegra.ph/file/cb58999f1510d1554e205.png" height="70" width="70" ><br>Wolf TV</h2>
-
+<div id="jtvh1">
+<a href="https://www.jio.com/en-in/apps/jio-tv">
+<h1>JioTV Web</h1>
+</div>
 </a>
-<br>
-<br>
-
-<hr>
-<a class="buttoncs" style="width:100%;padding:0px 0px 0px 0px;margin:0px;">
- <h2 style="font-size: 20px;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;">&nbsp;JOOLY TV - Genres 👇</h2>
-</a>
-<hr>
-
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './index2.php';">All Channels</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Sports.php';">Sports</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Entertainment.php';">Entertainment</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Movies.php';">Movies</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './News.php';">News</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Music.php';">Music</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Kids.php';">Kids</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Lifestyle.php';">Lifestyle</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Infotainment.php';">Infotainment</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Devotional.php';">Devotional</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Business.php';">Business</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Educational.php';">Educational</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Shopping.php';">Shopping</button>
-	 
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './JioDarshan.php';">JioDarshan</button>    
-
-
-
-
-
-
-<hr>
-<a class="buttoncs" style="width:100%;padding:0px 0px 0px 0px;margin:0px;">
- <h2 style="font-size: 20px;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;">&nbsp;JOOLY TV - Languages 👇</h2>
-</a>
-<hr>
-
-
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './index2.php';">All Channels</button>
-	 
-
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Hindi.php';">Hindi</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './English.php';">English</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Tamil.php';">Tamil</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Telugu.php';">Telugu</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Marathi.php';">Marathi</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Bengali.php';">Bengali</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Punjabi.php';">Punjabi</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Urdu.php';">Urdu</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Malayalam.php';">Malayalam</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Gujarati.php';">Gujarati</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Odia.php';">Odia</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Bhojpuri.php';">Bhojpuri</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Kannada.php';">Kannada</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Assamese.php';">Assamese</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './Nepali.php';">Nepali</button>    
-
-<button id="btn" type="button" class="neu-light btn-action" onclick="window.location.href = './French.php';">French</button>    
-
-
-
-
-<hr>
-<a class="buttoncs" href="https://cdn.wolftvOfficial.ga/jointelegram" style="width:100%;padding:0px 0px 0px 0px;margin:0px;">
- <h2 style="font-size: 20px;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;">&nbsp;Join Our Telegram Channel Now</h2>
-</a>
-<hr>
-
+<div id="content">
+<div class="container">
+<div id="list" class="row">
+<?php
+$json = json_decode(file_get_contents('channels.json') , true);
+foreach ($json['result'] as $channel) {
+echo '<div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2">'.PHP_EOL;
+printf("<a href=\"play.php?c=%s\" class=\"card\">".PHP_EOL, $channel['target']);
+printf("<img class=\"lazyload\" data-src=\"http://jiotv.catchup.cdn.jio.com/dare_images/images/%s\" style=\"height: 120px\">".PHP_EOL, $channel['logoUrl']);
+echo '<div class="card-body">'.PHP_EOL;
+printf("<p class=\"card-text\">%s</p>".PHP_EOL, $channel['channel_name']);
+echo '</div>'.PHP_EOL;
+echo '</a>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+}
+?>
+</div>
+</div>
+</div>
 </body>
 </html>
