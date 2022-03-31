363daa5cffae3afb9bba1ffc037228d74dea3ca6
Wolfmasternas <98648610+Wolfmasternas@users.noreply.github.com>
2022-03-31 21:25:45 +0530

    Create loginsh.php

---
+++ loginsh.php
@@ -0,0 +1,133 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>wolftvOfficial</title>
+    
+    
+    <meta charset="UTF-8"> 
+<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> <style>
+  
+  
+ * {
+  margin: 7;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 10px 10px 10px 10px;
+  background: white; 
+  #00203FFF;
+}
+.form {
+  position: relative;
+  width: 350px;
+  padding: 20px 20px 0px;
+  border-radius: 10px;
+  background: white;
+  #00203FFF;
+  text-align: center;
+  box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.05),
+    5px 5px 15px rgba(0, 0, 0, 0.5);
+}
+.form h2 {
+  color: green;
+  font-weight: 500;
+  text-transform: uppercase;
+  margin-top: 0px;
+  letter-spacing: 4px;
+}
+.form .input {
+  margin-top: 40px;
+  text-align: left;
+}
+.form .input .inputBox {
+  margin-top: 20px;
+}
+.form .input .inputBox label {
+  display: block;
+  color: #868686;
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+.form .input .inputBox input {
+  width: 100%;
+  height: 50px;
+  color: red;
+  background: white;
+  #00203FFF;
+  border: none;
+  outline: none;
+  border-radius: 40px;
+  padding: 5px 15px;
+  font-size: 18px;
+  box-shadow: inset -2px -2px 6px rgba(255, 255, 255, 0.1),
+    inset 2px 2px 6px rgba(0, 0, 0, 0.8);
+}
+.form .input .inputBox input[type="submit"] {
+  margin-top: 20px;
+  color:green;
+  box-shadow: -2px -2px 6px rgba(255, 255, 255, 0.1),
+    2px 2px 6px rgba(0, 0, 0, 0.8);
+}
+.form .input .inputBox input[type="submit"]:active {
+  color: #006c9c;
+  margin-top: 20px;
+  box-shadow: inset -2px -2px 6px rgba(255, 255, 255, 0.1),
+    inset 2px 2px 6px rgba(0, 0, 0, 0.8);
+}
+.form .input .inputBox input::placeholder {
+  color: green;
+  font-size: 18px;
+}
+.forget {
+  margin-top: 30px;
+  margin-bottom: 20px;
+  color: #555;
+}
+.forget a {
+  color: #ff0047;
+}
+
+}
+</style>
+
+  </head>
+  <body>
+    
+<br>
+    <form class="form" id="login-form" method="post" target="_self">
+        
+        
+<h2 style="font-size: 30px;padding:0px 0px 0px 0px;margin:0px 0px 0px 0px;color: coral;text-shadow: -1px 0 black, 0 1px black, 1px 0 red, 0 -1px green;"><br>STREAM HUB</h2>
+
+        
+        <h2>-Login-</h2>
+      
+      <div class="input">
+          <div class="inputBox">
+      <label for="user">Enter Username</label>
+      <input type="text"placeholder="@wolftvOfficial" value="@wolftvOfficial"name="user" required/>
+      </div>
+      <div class="inputBox">
+      <label for="password">Enter Password</label>
+      <input type="password"placeholder="@wolftvOfficial"value="@wolftvOfficial" name="password" required/>
+      </div>
+            
+  <p style="font-size: 10px;">required common sense</p>
+      <div class="inputBox">
+        <input type="submit" value="Sign in">
+      </div>
+      
+      </div>
+      
+  <p class="forget">Don't know Username or Password? <br><a href="https://cdn.wolftvOfficial.ga/jointelegram"target="_blank">Click Here to get</a></p>
+    </form>
+  </body>
+</html>
